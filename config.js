996c017ce9a408a92f36924c0d0f96a8a0a21ebd
Kaviexepavan <Pavanthathsara1@gmail.com>
2024-12-07 00:43:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "kp5XiJKB#kG4TCU1qbZXVVvUzQB6DUTttBjo8u0Nrdg5SsJ498DA",
+SESSION_ID: process.env.SESSION_ID || "B1wXjASD#7xxT_4aDD5UyrhutmIHU5QHnZy8gbVaeBTaehrTS8LY",
 ALIVE_REACT: process.env.ALIVE_REACT || "👋",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://pomf2.lain.la/f/pkuujaup.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "KAVI EXE_V1_Connected ☑️_📂𝙳𝙴𝙿𝙻𝚈 𝙱𝚈 KAVI_EXE📘☀",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
-ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
+ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "true",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
  AUTO_RECORDING: process.env.AUTO_RECORDING || "true",   
 };
